6ba40828654fedb0769ac1bdce26a87c0f25e95a
Fishdestroyer <jeffwhitner@gmail.com>
2022-02-24 23:17:37 -0600

    added notes to code

--- public/js/idb.js
+++ public/js/idb.js
@@ -3,6 +3,7 @@ let db;
 const request = indexedDB.open('WK-19-Budget-Tracker', 1);
 request.onupgradeneeded = function (event) {
 
+  // when db is successfully created with its object store (from onupgradedneeded event above), save reference to db in global variable
   const db = event.target.result;
 
   db.createObjectStore('new-transaction', { autoIncrement: true });
@@ -11,7 +12,7 @@ request.onupgradeneeded = function (event) {
 request.onsuccess = function (event) {
 
   db = event.target.result;
-
+//Verify the app is online
   if (navigator.onLine) {
 
     uploadTransaction();
@@ -27,19 +28,22 @@ function saveRecord(record) {
   const transaction = db.transaction(['new-transaction'], 'readwrite');
 
   const transactionObjectStore = transaction.objectStore('new-transaction');
-
+ // add record to your store with add method.
   transactionObjectStore.add(record);
 }
 
 
 function uploadTransaction() {
+  // open a transaction on your pending db
   const transaction = db.transaction(['new-transaction'], 'readwrite');
-
+// access your pending object store
   const transactionObjectStore = transaction.objectStore('new-transaction');
 
   const getAll = transactionObjectStore.getAll();
 
+  // get all records from store and set to a variable
   getAll.onsuccess = function () {
+    // if there was data in indexedDb's store, let's send it to the api server
     if (getAll.result.length > 0) {
       fetch('/api/transaction', {
         method: 'POST',
@@ -68,5 +72,5 @@ function uploadTransaction() {
     }
   };
 }
-
+//Upload transaction when connection is established
 window.addEventListener('online', uploadTransaction);
